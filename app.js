e6d068c23314d41de4cbad72c2172b9d1b1978b1
Pankaj <pankajgupta28427@gmail.com>
2021-10-14 00:35:25 +0530

    Errors fixed and My-Memories button added

--- app.js
+++ app.js
@@ -14,48 +14,49 @@ const Memory = require('./models/memory');
 
 const app = express();
 
-const DB="mongodb+srv://admin-pankaj:Pg01062001@cluster0.ktsqd.mongodb.net/userDB";
-//GITHUBBBBB
+const DB = "mongodb+srv://admin-pankaj:Pg01062001@cluster0.ktsqd.mongodb.net/userDB";
+
 //database= username=admin-pankaj
 //                  password=Pg01062001
 // display "wrong id or password try again" when wrong data  //USE FLASH
 // display invalid username or password when invalid data      //USE FLASH
 // IMPROVE css+looks(all pages)
-//use oAuth to get google button
-//use joi to validate memo-form data
+//use joi to validate memo-form data  (ERRORRRR)
 //put delete button on (my memories) page cards
-//check if async await can be used or not to load cards(all PAGES )
-//my-memories loading early before code running
+//MULTER to load images(Add a default pic)
+//SEARCH bar
+//Memory scehma not taking duplicate entries
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: "Memories Fun app",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+const { storage } = require('./utils/cloudinary')
+const upload = multer({ storage });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, 'public/images/memory-images'));
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
-    }
-});
-
-const upload = multer({ storage: storage })
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.join(__dirname, 'public/images/memory-images'));
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + "-" + file.originalname);
+//     }
+// });
+// const upload = multer({ storage: storage })
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false,
     useUnifiedTopology: true
-  }).then(() => console.log('Connection to DB established'));
+}).then(() => console.log('Connection to DB established'));
 
 passport.use(User.createStrategy());
 
@@ -111,6 +112,7 @@ app.post("/login", function (req, res) {
     });
     passport.authenticate("local", { failureRedirect: '/login', failureFlash: true })(req, res, function () {
         res.redirect("/memories");
+        //FLASH error when fail
     });
 });
 
@@ -153,86 +155,124 @@ app.get("/submit", function (req, res) {
 })
 
 app.post("/submitmemo", upload.single('memo-image'), function (req, res) {
- 
-    let JoiSchema = joi.object({
-        creator: joi.string().min(3).max(20).required(),
-        title: joi.string().min(4).max(40),
-        message: joi.string().min(10).max(80)
-    });
-    
-    let result = JoiSchema.validate(req.body);
-    if (result.error) {
-        res.redirect("/submit");
-        console.log(result + "      User Error=" + result.error.details[0].message);
-        //FLASH
-    }
-    else {
-        result.save(function (err) {
-            if (err) {
-                res.send(err);
-            } else {
-                User.findById(req.user.id, function (err, foundUser) {
-                    foundUser.userMemory.push(req.body);
-                    foundUser.save(function () {
-                        res.redirect("/memories");
-                    });
-                });
-            }
-        });
-    }
 
-    // const newMemo = new Memory({
-    //     creator: req.body.creator,
-    //     title: req.body.title,
-    //     message: req.body.memory,
+    // let JoiSchema = joi.object({
+    //     creator: joi.string().min(3).max(20).required(),
+    //     title: joi.string().min(4).max(40).required(),
+    //     message: joi.string().min(10).max(100).required()
     // });
 
-    // newMemo.save(function (err) {
-    //     if (err) {
-    //         res.send(err);
-    //     } else {
-    //         User.findById(req.user.id, function (err, foundUser) {
-    //             foundUser.userMemory.push(newMemo);
-    //             foundUser.save(function () {
-    //                 res.redirect("/memories");
+    // let result = JoiSchema.validate(req.body);
+    // if (result.error) {
+    //     res.redirect("/submit");
+    //     console.log(result.error.details[0].message);
+    //     //FLASH
+    // }
+    // else {
+    //   
+    //     result.save(function (err) {
+    //         if (err) {
+    //             res.send(err);
+    //         } else {
+    //             User.findById(req.user.id, function (err, foundUser) {
+    //                 foundUser.userMemory.push(req.body);
+    //                 foundUser.save(function () {
+    //                     res.redirect("/memories");
+    //                 });
     //             });
-    //         });
-    //     }
-    // });
-});
+    //         }
+    //     });
+    // }
 
-
-app.get("/memories/my-memories", function (req, res) {
-    let userMemories = [];
-    let headingText="";
-    User.findById(req.user.id, function (err, foundUser) {
+    const newMemo = new Memory({
+        creator: req.body.creator,
+        title: req.body.title,
+        message: req.body.memory,
+        imagePath: req.file.path,
+    });
+    console.log(newMemo);
+    newMemo.save(function (err) {
         if (err) {
             console.log(err);
         } else {
-            const memoryIds = foundUser.userMemory;
-            console.log("user memory IDS=" + memoryIds);
-
-            if (memoryIds.length == 0) {
-                headingText="No Memories Saved :(";
-                //display no memory found, give create option 
-            } else {
-                headingText="My Memories";
-                memoryIds.forEach((memoId) => {
-                    Memory.findById(memoId, function (err, result) {                  //loop
-                        userMemories.push(result);
-                        console.log("UserMemories in loop=" + userMemories);
-                    });
+            User.findById(req.user.id, function (err, foundUser) {
+                foundUser.userMemory.push(newMemo);
+                foundUser.save(function () {
+                    res.redirect("/memories");
                 });
-            }
+            });
         }
     });
-    console.log("UserMemories=" + userMemories);
-    res.render("memories", { userWithMemories: userMemories, headingMemo: headingText });
-    
 });
+
+
+let fetchMemories = async (ids) => {
+    try {
+        let arr = []
+        for (let userID of ids) {
+            let foundMemory = await Memory.find({ "_id": userID });
+            arr.push(foundMemory[0]);
+        }
+        return arr
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+app.get("/memories/my-memories", async function (req, res) {
+
+    let headingText = "";
+    let userMemories = [];
+    try {
+        const foundUser = await User.findById(req.user.id);
+        const memoryIds = foundUser.userMemory;
+
+        if (memoryIds.length == 0) {
+            headingText = "No Memories Saved";
+            //display no memory found, give create option 
+        } else {
+            headingText = "My Memories";
+            userMemories = await fetchMemories(memoryIds);
+
+        }
+        res.render("memories", { userWithMemories: userMemories, headingMemo: headingText });
+    }
+    catch (e) {
+        console.log(e);
+    }
+});
+
+
+// app.get("/memories/my-memories", function (req, res) {
+//     let userMemories = [];
+//     let headingText = "";
+
+//     User.findById(req.user.id, function (err, foundUser) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             const memoryIds = foundUser.userMemory;
+//             console.log("user memory IDS=" + memoryIds);
+//             if (memoryIds.length == 0) {
+//                 headingText = "No Memories Saved :(";
+//                 //display no memory found, give create option 
+//             } else {
+//                 headingText = "My Memories";
+//                 console.log(headingText);
+//                 memoryIds.forEach((memoId) => {
+//                     Memory.findById(memoId, function (err, result) {                  //loop
+//                         userMemories.push(result);
+//                         console.log("UserMemories in loop=" + userMemories);
+//                     });
+//                 });
+//             }
+//         }
+//         console.log("UserMemories=" + userMemories);
+//     });
+//     res.render("memories", { userWithMemories: userMemories, headingMemo: headingText });
+// });
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
-
-
--- models/memory.js
+++ models/memory.js
@@ -2,32 +2,14 @@ const mongoose = require("mongoose");
 const user = require("./user");
 
 const memorySchema = new mongoose.Schema({
-    creator: {
-        type: String,
-        unique: true,
-        min: 5,
-        max: 30
-    },
-    title: {
-        type: String,
-        min: 5,
-        max: 30
-    },
-    message:  {
-        type: String,
-        minlength:5,
-        maxlength:80
-      },
-    // imageName: String 
+    creator: String,
+    title: String,
+    message: String,
+    imagePath: String,
     userData: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }
 });
 
-
 module.exports = mongoose.model("Memory", memorySchema);
-
-// creator: joi.string().min(5).max(30),
-//         title: joi.string().min(5).max(30),
-//         message: joi.string().min(5).max(80),
